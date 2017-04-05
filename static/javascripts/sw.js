deb23418e8c603daf2a4484d6fe2dc85803c220c
Arnelle Balane <arnellebalane@gmail.com>
2017-04-05 10:11:14 +0800

    bump service worker cache version

--- static/javascripts/sw.js
+++ static/javascripts/sw.js
@@ -2,7 +2,7 @@ importScripts('/offline-google-analytics/offline-google-analytics-import.js');
 goog.offlineGoogleAnalytics.initialize();
 
 
-const cacheName = 'gcpnext17-v2';
+const cacheName = 'gcpnext17-v3';
 const pathsToCache = [
   '/',
   '/faqs',
