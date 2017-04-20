def305252d1fcc2e803e4214aa2873062e2aae40
Arnelle Balane <arnellebalane@gmail.com>
2017-04-20 11:26:24 +0800

    add partner talk to agenda

--- static/javascripts/sw.js
+++ static/javascripts/sw.js
@@ -2,7 +2,7 @@ importScripts('/offline-google-analytics/offline-google-analytics-import.js');
 goog.offlineGoogleAnalytics.initialize();
 
 
-const cacheName = 'gcpnext17-v7';
+const cacheName = 'gcpnext17-v8';
 const pathsToCache = [
   '/',
   '/faqs',
